92e3b3023d178ca653db3fb11ae72189c26cb29c
Aitor Iglesias <iglesiaspubillaitor@gmail.com>
2023-04-06 13:42:47 +0200

    Ejercicio 8 Creado. Ejercicio 7 corregido

--- JavaModC4Maven/src/main/java/com/aiglesiasp/maven/JavaModC4Maven/ejercicio8/Ejercicio8.java
+++ JavaModC4Maven/src/main/java/com/aiglesiasp/maven/JavaModC4Maven/ejercicio8/Ejercicio8.java
@@ -1,29 +1,36 @@
-package com.aiglesiasp.maven.JavaModC4Maven.ejercicio7;
+/**
+ * 
+ */
+package com.aiglesiasp.maven.JavaModC4Maven.ejercicio8;
 
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Ejercicio7 extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
 
+/**
+ * @author aitor
+ *
+ */
+public class Ejercicio8 extends JFrame {
 	private JPanel contentPane;
 	private JTextField fieldCantidad;
 	private JLabel fieldResultado;
 	private boolean estado = true;
 	private JButton btnConversor;
 	private final double EURO = 166.386;
+	private JButton btnLimpiar;
 	private JButton btnCambiar;
-
+	
 	/**
 	 * Create the frame.
 	 */
-	public Ejercicio7() {
+	public Ejercicio8() {
 		setTitle("EJERCICIO 7");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(300, 300, 450, 300);
@@ -60,34 +67,44 @@ public class Ejercicio7 extends JFrame {
 		btnCambiar.setBounds(113, 126, 117, 29);
 		contentPane.add(btnCambiar);
 
+		btnLimpiar = new JButton("LIMPIAR");
+		btnLimpiar.addActionListener(al);
+		btnLimpiar.setBounds(242, 126, 117, 29);
+		contentPane.add(btnLimpiar);
+
 		setVisible(true);
 	}
-
+	
 	// ACTION LISTENER
-	ActionListener al = new ActionListener() {
-		public void actionPerformed(ActionEvent e) {
-			// Boton conversor
-			if (e.getSource() == btnConversor) {
-				double num = Double.parseDouble(fieldCantidad.getText());
-				double res = 0.0;
-				if (estado == true) {
-					res = num * EURO;
-				} else {
-					res = num / EURO;
+		ActionListener al = new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Boton conversor
+				if (e.getSource() == btnConversor) {
+					double num = Double.parseDouble(fieldCantidad.getText());
+					double res = 0.0;
+					if (estado == true) {
+						res = num * EURO;
+					} else {
+						res = num / EURO;
+					}
+					fieldResultado.setText("" + res);
 				}
-				fieldResultado.setText("" + res);
-			}
 
-			if (e.getSource() == btnCambiar) {
-				if (estado == true) {
-					btnConversor.setText("Ptas a euros");
-					estado = false;
-				} else {
-					btnConversor.setText("Euros a ptas");
-					estado = true;
+				if (e.getSource() == btnCambiar) {
+					if (estado == true) {
+						btnConversor.setText("Ptas a euros");
+						estado = false;
+					} else {
+						btnConversor.setText("Euros a ptas");
+						estado = true;
+					}
 				}
-			}
-		}
-	};
 
+				if (e.getSource() == btnLimpiar) {
+					fieldResultado.setText("");
+					fieldCantidad.setText("");
+				}
+
+			}
+		};
 }
